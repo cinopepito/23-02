3b32274e34373b4a51f91d537df9a1a96930d6f1
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-24 21:18:16 +0100

    new line have been added to my code

---
+++ dom.js
@@ -0,0 +1,7 @@
+let sentence = "broly is the best to answer the question";
+let partOfsentence = sentence.slice(0, 2);
+let myName = "henri";
+let firsCharacter = myName.slice(0, 1);
+let restOfName = myName.slice(1, myName.length);
+console.log(restOfName);
+console.log(firsCharacter.toUpperCase() + restOfName);
---
+++ scope.js
@@ -0,0 +1,22 @@
+//global scope
+let food = "apple";
+let fruit = 5;
+let x = 10;
+// console.log(x);
+// //function scope
+// function store() {
+//   let food = "banana";
+//   let fruits = 50;
+//   let x = 5;
+//   console.log(x);
+// }
+// store();
+// // block scope
+// for (let x = 0; x < 10; x++) {
+//   console.log(x);
+// }
+if (true) {
+  let food = "kiwi";
+  let x = 10;
+}
+
