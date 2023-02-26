afa9781edb63fe5dd7204f58524d04c5e1f645a0
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-26 09:00:53 +0100

    loops has been modified

--- loop.js
+++ loop.js
@@ -22,3 +22,62 @@ function whoisPaying() {
   return randomPerson + " is going to buy launch today";
 }
 console.log(whoisPaying());
+
+let list = ["milk", "bread", "apple"];
+list.splice(1, 1, "Bananas", "Eggs");
+list.pop();
+list.sort();
+list.splice(1, 0, "carrot", "lettuce");
+console.log(list.lastIndexOf("milk"));
+console.log(list);
+let newlist = ["juice", "pop"];
+let arr = list.concat(newlist);
+console.log(arr);
+let lastIndex = arr.lastIndexOf("pop");
+console.log(lastIndex);
+
+let mylist = [1, 2, 3];
+let mylist2 = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+console.log(mylist2[0][1]);
+
+const theList = [
+  "Laurence",
+  "Svekis",
+  true,
+  35,
+  null,
+  undefined,
+  { test: "one", score: 55 },
+  ["one", "two"],
+];
+theList.shift();
+theList.pop();
+theList.unshift("FIRST");
+theList[4] = "hello world";
+theList[3] = "MIDDLE";
+theList.push("LAST");
+console.log(theList);
+
+let inventory = [
+  (item = {
+    name: "brose",
+    model: "long",
+    quantity: 10,
+  }),
+  (item2 = {
+    name: "couteau",
+    model: "large",
+    quantity: 100,
+  }),
+  (item3 = {
+    name: "tirebouchon",
+    model: "plate",
+    quantity: 200,
+  }),
+];
+inventory[1].name = "barre de fer";
+console.log(inventory);
