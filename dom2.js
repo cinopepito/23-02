9511799ef6fae9a829875c25cf024b1db6edf675
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-26 11:55:20 +0100

    dom2 has been added to the file

---
+++ dom2.js
@@ -0,0 +1,4 @@
+let heading = document.querySelector("h1");
+heading.classList.add("huge");
+console.log(document.querySelector("a").attributes);
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
